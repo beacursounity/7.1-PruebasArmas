cd9f9135f92d2ca14ba5651742d40368f2a6fe6f
beacursounity <43536093+beacursounity@users.noreply.github.com>
2021-01-23 18:22:29 +0100

    Modificaciones
    Trail Renderer de la bala del Mortero

--- Assets/_GameAssets/Scripts/MorteroScript.cs
+++ Assets/_GameAssets/Scripts/MorteroScript.cs
@@ -6,18 +6,38 @@ public class MorteroScript : MonoBehaviour {
 
     [SerializeField] Transform genPoint;
     [SerializeField] GameObject prefabProyectil;
+    //[SerializeField] GameObject hitParticles;
     [SerializeField] int force = 600;
 
+    //[SerializeField] LineRenderer balaTrail;
+    
+
+    //public float range = 100f; // MAXIMO RANGO ARMA
+    //float spreadfactor = 0.1f;
+
+    //RaycastHit hit;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private void FixedUpdate()
+    {
+        //Physics.Raycast(genPoint.transform.position, proyectil.transform.forward,
+         ///       out hit);
+    }
+
+    /*void GeneracionTrailBalas (Vector3 puntoGolpe) {
+        GameObject efectoTrailBala = Instantiate(balaTrail.gameObject,
+            genPoint.transform.position, Quaternion.identity);
+
+        LineRenderer lineaR = efectoTrailBala.GetComponent<LineRenderer>();
+        lineaR.SetPosition(0, genPoint.transform.position);
+        lineaR.SetPosition(1, puntoGolpe);
+
+        Destroy(efectoTrailBala,1f);
+	}*/
 
     // cuando pulsemos con el raton encima de nuestro Mortero disparara
     private void OnMouseDown() {
@@ -27,6 +47,22 @@ public class MorteroScript : MonoBehaviour {
 
         //LE AÑADIMOS LA FUERZA
         proyectil.GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * force);
+
+        /*Vector3 direccion = genPoint.transform.forward;
+        direccion = direccion + genPoint.TransformDirection
+                (new Vector3(Random.Range(-spreadfactor, spreadfactor),
+                Random.Range(-spreadfactor, spreadfactor)));
+
+        if (Physics.Raycast(genPoint.position, direccion,
+                out hit , range)){
+            print("Ha pasado");
+            GameObject hitParticleEffect = Instantiate(hitParticles, hit.point,
+                Quaternion.FromToRotation(Vector3.up, hit.normal));
+
+           // GameObject bullImpact = Instantiate();
+        }
+
+        GeneracionTrailBalas(hit.point);*/
 
         // DESTRUIMOS EL PROYECTIL
        // Invoke("Destruir", 2);
